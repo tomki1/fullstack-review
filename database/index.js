6f9c86301ef333c63fe62b391bcfeac179e98965
Kimberly Tom <kimberlyjtom@gmail.com>
2023-07-31 17:13:22 -0700

    hardcoded insert into mongodb Fetcher DB works

--- database/index.js
+++ database/index.js
@@ -1,16 +1,44 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+const db = mongoose.connection;
+// determine whether there is a db connection error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to mongoDB fetcher database");
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+let repoSchema = mongoose.Schema({
+  repo_id: {type: Number, unique: true},
+  repo_name: String,
+  repo_url: String,
+  user_id: Number,
+  username: String,
+  stargazers_count: Number
+});
 
-let save = (/* TODO */) => {
+// let userSchema = mongoose.Schema({
+//   user_id: {type: Number, unique: true},
+//   username: String,
+//   repos: [{type: Schema.Types.ObjectId, ref: 'Repo'}]
+// })
+
+let Repo = mongoose.model('Repo', repoSchema);
+// let User = mongoose.model('User', userSchema);
+
+let save = (error, result) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(result);
+  }
+
+
 }
 
 module.exports.save = save;
+module.exports.db = db;
+module.exports.Repo = Repo;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,57 @@
 const express = require('express');
 let app = express();
+var path = require('path');
+const db = require('../database/index.js');
+const Promise = require('bluebird');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
 
-app.post('/repos', function (req, res) {
+var location = path.join(__dirname, '../client/dist');
+
+app.use(express.static(location));
+
+
+app.post('/repos', async function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  const repo = new db.Repo({
+    repo_id: 43245343,
+    repo_name: 'fullstack-review',
+    repo_url: 'www.github.com/fs',
+    user_id: 335545,
+    username: 'coolname',
+    stargazers_count: 3
+  });
+  console.log("in post")
+  try {
+    repo.save() // test with hardcoded
+    .then( item => {
+      console.log(item);
+      res.send("saved to db");
+      }
+    )
+    .catch( err => {
+      res.status(400).send("unable to save to db");
+    });
+
+  } catch(error) {
+    res.status(500).json({error: e.message});;
+
+  }
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  res.send(repo);
 });
 
 let port = 1128;
